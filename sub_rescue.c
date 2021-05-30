b3e15ecda7626ba8aab620be0b389ba7de5aa9d6
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-05-30 20:01:42 -0300

    Version 0.3

--- sub_rescue.c
+++ sub_rescue.c
@@ -1058,6 +1058,6 @@ void main() {
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
-SMS_EMBED_SDSC_HEADER(0,2, 2021,3,21, "Haroldo-OK\\2021", "Sub Rescue",
+SMS_EMBED_SDSC_HEADER(0,3, 2021,5,30, "Haroldo-OK\\2021", "Sub Rescue",
   "A subaquatic shoot-em-up.\n"
   "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
